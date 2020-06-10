2dc53fdd3bd7ee45e6a53b04d4e8268814ab19c1
tulenscript <bkmzag20041@list.ru>
2020-06-10 12:31:55 +0400

    Beautify script

--- user-agents.rb
+++ user-agents.rb
@@ -5,6 +5,6 @@ require "http"
 resp = HTTP.post("https://user-agents.net/random", 
                  form: { "limit": ARGV[0], "action": "generate" }).to_s
 
-resp.to_s.scan(/<li><a href="\/string.*">(.*)<\/a><\/li>/).each do |match|
+resp.scan(/<li><a href="\/string.*">(.*)<\/a><\/li>/).each do |match|
   puts match
 end
